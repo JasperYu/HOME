319ec784c4296f339302f8a66f019403fcf88fd0
JasperYu <jpyu@mail.ustc.edu.cn>
2020-02-09 02:05:59 +0800

    c++ add

---
+++ c++/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sugus chan on 2020-02-09.
+//
+
+#include "main.h"
---
+++ c++/main.h
@@ -0,0 +1,14 @@
+//
+// Created by sugus chan on 2020-02-09.
+//
+
+#ifndef HOME_MAIN_H
+#define HOME_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //HOME_MAIN_H
